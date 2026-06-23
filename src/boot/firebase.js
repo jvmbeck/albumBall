9fba31654aa5a5b9d111629180e8859e30b31529
jvmbeck <jmadridbeck@gmail.com>
2026-06-23 08:53:28 -0300

    firebase config update

--- src/boot/firebase.js
+++ src/boot/firebase.js
@@ -1,7 +1,7 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from 'firebase/app'
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { boot } from 'quasar/wrappers'
+import { getApp, getApps, initializeApp } from 'firebase/app'
+import { getFirestore } from 'firebase/firestore'
+import { getStorage } from 'firebase/storage'
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
@@ -15,7 +15,12 @@ const firebaseConfig = {
   measurementId: 'G-7NLZRZW040',
 }
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig)
+const firebaseApp = getApps().length ? getApp() : initializeApp(firebaseConfig)
+const firestoreDb = getFirestore(firebaseApp)
+const firebaseStorage = getStorage(firebaseApp)
 
-export default app
+export { firebaseApp, firestoreDb, firebaseStorage }
+
+export default boot(() => {
+  // Firebase is initialized by importing this boot file.
+})
